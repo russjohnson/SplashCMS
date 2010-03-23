1ec07c577205b1c1cc9afb1140bcccb728a5f32d
Raul Riera <rieraraul@gmail.com>
2010-03-23 01:00:37 -0430

    Removed the "INSTALL" folder, it was doing more harm than good. Long live DBMigrate :)

--- INSTALL/SQL/change_fileName_column_size_mysql.sql
+++
@@ -1,8 +0,0 @@
-// modify the column names in order to accept the new UUID value (plus the string ".cfm")
-
-alter table pageparts modify fileName VARCHAR(39);
-
-alter table layouts modify fileName VARCHAR(39);
-
-// this is not needed (for some reason was created in "INSTALL/SQL/add_fileName_column_mysql.sql"
-// alter table pages modify fileName VARCHAR(39);
